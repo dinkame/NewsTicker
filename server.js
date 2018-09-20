995b54a2183f6c549becae18c2b90cf70a2fb808
Dina <dinkan@gmail.com>
2018-09-20 21:19:38 +0300

    clean port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
-var port = process.env.PORT || process.env.port || 8080;
+var port = process.env.port;
 
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
